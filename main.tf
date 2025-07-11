ee16e57bd22c6f59a3ac9ed1af5ebe4346518451
Jesus-DelRiego <jesus.delriego@hashicorp.com>
2025-07-11 15:25:52 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "null_resource" "previous" {}
 resource "time_sleep" "wait_300_seconds" {
   depends_on = [null_resource.previous]
 
-  create_duration = "3s"
+  create_duration = "30s"
 }
 
 # This resource will create (at least) 300 seconds after null_resource.previous
